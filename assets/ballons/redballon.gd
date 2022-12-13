8e4b4e376e43881d9e64721d3efb9c390f18c777
jacob-vkd <67998179+jacob-vkd@users.noreply.github.com>
2022-12-13 14:30:12 +0100

    [REF] Refactoring

--- assets/ballons/redballon.gd
+++ assets/ballons/redballon.gd
@@ -5,16 +5,21 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
-
+var sound_played = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	print("SCALE.X = ")
-	print(scale.x)
-	if scale.x > 1.3:
-		visible = false
+	
+	#POP Ballon with sound
+	if sound_played == false:
+		if scale.x > 1.3:
+			visible = false
+			if get_node("AudioStreamPlayer").playing == false:
+				get_node("AudioStreamPlayer").play()
+				sound_played = true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
--- assets/tools/Pump3.gd
+++ assets/tools/Pump3.gd
@@ -1,54 +1,48 @@
 extends KinematicBody
 
 var state_machine
-var timer = Timer
-
-func _ready():
-	state_machine = $AnimationTree.get("parameters/playback")
 	
 func _physics_process(delta):
-	get_input()
-	
+	if is_network_master():
+		get_input()	
+
 	
 func get_input():
 	var current = state_machine.get_current_node()
+	var ballon = get_node("bicyclePump/redballon")
 	
-	if Input.is_action_just_pressed("ui_up"):
-		print("INPUT IS UP")
-		#if pump is on half and up was pressed
-		if current == "TopToHalf001":
-			state_machine.travel("HalfToUp002")
+	if ballon.visible:
+		if Input.is_action_just_pressed("ui_up"):
+			#if pump is on half and up was pressed
+			if current == "TopToHalf001":
+				state_machine.travel("HalfToUp002")
+				return
+			#if pump is bot and up was pressed
+			if current == "HalfToBot001":
+				state_machine.travel("BotToHalf001")
+				return
+			if current == "BotToHalf001":
+				state_machine.travel("HalfToUp002")
+				return
 			return
-		#if pump is bot and up was pressed
-		if current == "HalfToBot001":
-			state_machine.travel("BotToHalf001")
-			return
-		if current == "BotToHalf001":
-			state_machine.travel("HalfToUp002")
-			return
-		print("Pump is allready up bro")
-		return
 
-	
-	if Input.is_action_just_pressed("ui_down"):
-		print("INPUT IS DOWN")
-		print(current)
-		# if pump is on half and down was pressed
-		if current == "TopToHalf001":
-			#move to bot
-			state_machine.travel("HalfToBot001")
-			get_node("redballon").scale *=1.04
+	if ballon.visible:
+		if Input.is_action_just_pressed("ui_down"):
+			# if pump is on half and down was pressed
+			if current == "TopToHalf001" || current == "BotToHalf001":
+				#move to bot
+				state_machine.travel("HalfToBot001")
+				ballon.scale *=1.04
+				return
+			if current == "HalfToBot001":
+				return
+			#else move from top to middle
+			state_machine.travel("TopToHalf001")
+			ballon.scale *=1.04
 			return
-		if current == "BotToHalf001":
-			state_machine.travel("HalfToBot001")
-			get_node("redballon").scale *=1.04
-			return
-		if current == "HalfToBot001":
-			print("Pump is allready down bro")
-			return
-		#else move from top to middle
-		state_machine.travel("TopToHalf001")
-		get_node("redballon").scale *=1.04
-		return
 		
 		
+
+
+func _on_Network_tick_rate_timeout():
+	pass # Replace with function body.
